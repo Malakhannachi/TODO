1e71087f75e4977ec0d997927e7be8b161730507
Malak Hannachi <malakhannachi@gmail.com>
2024-04-03 16:32:58 +0200

    javascript

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 const addBtn  = document.querySelector('#btn');
 const taskCard = document.querySelector(".todoCard");
 const tasksContainer = document.querySelector("#todoCards");
+const counter =document.querySelector(".compteur")
+
 
 const delBtn = document.querySelector('.delBtn');
 delBtn.addEventListener('click',function(){
@@ -13,17 +15,26 @@ addBtn.addEventListener('click', addTask);
 
 function addTask() {
     const newTask = taskCard.cloneNode(true);
-    const newDelBtn = newTask.querySelector('.delBtn')
-    const newTextArea = newTask.querySelector('task')
-    newTextArea.value = "New Task"
+    const newDelBtn = newTask.querySelector('.delBtn');
+    const newTextArea = newTask.querySelector('.task');
+    newTextArea.value = "New Task";
     newDelBtn.addEventListener('click',function () {
         deleteTask(newTask);
         
     })
     tasksContainer.appendChild(newTask);
-    updateCount();
+    updateCount()
 }
+function updateCount(){
+    const todoCards = document.querySelector("#todoCards");
+        counter.innerHTML = todoCards.children.length;
+        console.log(counter);
+    }
+
+
 function deleteTask(task) {
     task.remove();
     
 }
+
+updateCount();
